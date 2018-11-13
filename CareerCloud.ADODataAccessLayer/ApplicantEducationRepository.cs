b34ab7d951c18526189b24491d46a54587552ddd
Anoo <naray@LAPTOP-BLF2U6N6>
2018-11-13 15:11:34 -0500

    Created the BaseADO class,inherited in ApplicantEducationRepository and implemented the Add and GetAll function of ApplicantEducationRepository

--- CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
+++ CareerCloud.ADODataAccessLayer/ApplicantEducationRepository.cs
@@ -2,6 +2,7 @@
 using CareerCloud.Pocos;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -9,11 +10,31 @@ using System.Threading.Tasks;
 
 namespace CareerCloud.ADODataAccessLayer
 {
-    public class ApplicantEducationRepository : IDataRepository<ApplicantEducationPoco>
+    public class ApplicantEducationRepository : BaseADO,IDataRepository<ApplicantEducationPoco>
     {
         public void Add(params ApplicantEducationPoco[] items)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand();
+                command.Connection = conn;
+                foreach (ApplicantEducationPoco poco in items)
+                {
+                    command.CommandText = @"INSERT INTO [dbo].[Applicant_Educations] ([Id],[Applicant],[Major],[Certificate_Diploma],[Start_Date],[Completion_Date],[Completion_Percent]) VALUES (@Id,@Applicant,@Major,@Certificate_Diploma,@Start_Date,@Completion_Date,@Completion_Percent) ";
+                    command.Parameters.AddWithValue("@Id", poco.Id);
+                    command.Parameters.AddWithValue("@Applicant", poco.Applicant);
+                    command.Parameters.AddWithValue("@Major", poco.Major);
+                    command.Parameters.AddWithValue("@Certificate_Diploma", poco.CertificateDiploma);
+                    command.Parameters.AddWithValue("@Start_Date", poco.StartDate);
+                    command.Parameters.AddWithValue("@Completion_Date", poco.CompletionDate);
+                    command.Parameters.AddWithValue("@Completion_Percent", poco.CompletionPercent);
+                    conn.Open();
+                    int rowEffected = command.ExecuteNonQuery();
+                    conn.Close();
+
+                }
+               
+            }
         }
 
         public void CallStoredProc(string name, params Tuple<string, string>[] parameters)
@@ -23,7 +44,51 @@ namespace CareerCloud.ADODataAccessLayer
 
         public IList<ApplicantEducationPoco> GetAll(params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
         {
-            
+            ApplicantEducationPoco[] pocos = new ApplicantEducationPoco[500];            
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand command = new SqlCommand("select * from Applicant_Educations", conn);
+                int position = 0;
+                SqlDataReader reader = command.ExecuteReader();
+                while(reader.Read())
+                {
+                    ApplicantEducationPoco poco = new ApplicantEducationPoco();
+                    poco.Id = reader.GetGuid(0);
+                    poco.Applicant = reader.GetGuid(1);
+                    poco.Major = reader.GetString(2);
+                    poco.CertificateDiploma = reader.GetString(3);
+                    if(!reader.IsDBNull(4))
+                    {
+                        poco.StartDate = reader.GetDateTime(4);
+                    }
+                    else
+                    {
+                        poco.StartDate = null;
+                    }
+
+                    if (!reader.IsDBNull(5))
+                    {
+                        poco.CompletionDate = reader.GetDateTime(5);
+                    }
+                    else
+                    {
+                        poco.CompletionDate = null;
+                    }
+                    if (!reader.IsDBNull(6))
+                    {
+                        poco.CompletionPercent = reader.GetByte(6);
+                    }
+                    else
+                    {
+                        poco.CompletionPercent = null;
+                    }
+                    poco.TimeStamp = (byte[]) reader[7];
+                    pocos[position] = poco;
+                    position++;
+                }
+
+            }
+            return pocos.ToList();
         }
 
         public IList<ApplicantEducationPoco> GetList(Expression<Func<ApplicantEducationPoco, bool>> where, params Expression<Func<ApplicantEducationPoco, object>>[] navigationProperties)
---
+++ CareerCloud.ADODataAccessLayer/BaseADO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CareerCloud.ADODataAccessLayer
+{
+    public abstract class BaseADO
+    {
+        protected string connString;
+
+        public BaseADO()
+        {
+            connString = @"Data Source = LAPTOP-BLF2U6N6\HUMBERBRIDGING; InitialCatalog = JOB_PORTAL_DB; Integrated Security = True;";
+        }
+    }
+}

6e751fb7c65aacddc90e51e425c1f296d4566b87
Anoo <naray@LAPTOP-BLF2U6N6>
2018-12-06 22:22:59 -0500

    Created New Class Library CarrerCloud.BusinessLogicLayer and created ValidateException,BasicLogic Class,created Logic classes for all the Poco classes and inherited Basic Logic class,created constructor for all except for systemcountry code and system languagecode logic classes

---
+++ CareerCloud.BusinessLogicLayer/ApplicantJobApplicationLogic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CareerCloud.Pocos;
+using CareerCloud.DataAccessLayer;
+
+namespace CareerCloud.BusinessLogicLayer
+{
+    public class ApplicantJobApplicationLogic : BaseLogic<ApplicantJobApplicationPoco>
+    {
+        public ApplicantJobApplicationLogic(IDataRepository<ApplicantJobApplicationPoco> repository) : base(repository)
+        {
+        }
+    }
+}

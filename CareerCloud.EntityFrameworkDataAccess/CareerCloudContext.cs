dde538335433aecb8062a3026952b2ae95b7ca51
Anoo <naray@LAPTOP-BLF2U6N6>
2019-02-03 00:20:29 -0500

    Added changed to carrer cloud context

--- CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
+++ CareerCloud.EntityFrameworkDataAccess/CareerCloudContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,10 +20,29 @@ namespace CareerCloud.EntityFrameworkDataAccess
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Entity<ApplicantProfilePoco>().HasMany(a => a.ApplicantEducations).WithRequired(e => e.ApplicantProfiles).HasForeignKey(e => e.Applicant);
+            modelBuilder.Entity<ApplicantProfilePoco>().HasMany(a => a.ApplicantJobApplications).WithRequired(e => e.ApplicantProfiles).HasForeignKey(e => e.Applicant);            
+            modelBuilder.Entity<ApplicantProfilePoco>().HasMany(a => a.ApplicantResumes).WithRequired(e => e.ApplicantProfiles).HasForeignKey(e => e.Applicant);
+            modelBuilder.Entity<ApplicantProfilePoco>().HasMany(a => a.ApplicantSkills).WithRequired(e => e.ApplicantProfiles).HasForeignKey(e => e.Applicant);
+            modelBuilder.Entity<ApplicantProfilePoco>().HasMany(a => a.ApplicantWorkHistories).WithRequired(e => e.ApplicantProfiles).HasForeignKey(e => e.Applicant);
+            
+            modelBuilder.Entity<CompanyJobPoco>().HasMany(a => a.ApplicantJobApplications).WithRequired(e => e.CompanyJobs).HasForeignKey(e => e.Job);
+            modelBuilder.Entity<CompanyJobPoco>().HasMany(a => a.CompanyJobDescriptions).WithRequired(e => e.CompanyJobs).HasForeignKey(e => e.Job);
+            modelBuilder.Entity<CompanyJobPoco>().HasMany(a => a.CompanyJobSkills).WithRequired(e => e.CompanyJobs).HasForeignKey(e => e.Job);
+            modelBuilder.Entity<CompanyJobPoco>().HasMany(a => a.CompanyJobEducations).WithRequired(e => e.CompanyJobs).HasForeignKey(e => e.Job);
 
+            modelBuilder.Entity<CompanyProfilePoco>().HasMany(a => a.CompanyDescriptions).WithRequired(e => e.CompanyProfiles).HasForeignKey(e => e.Company);
+            modelBuilder.Entity<CompanyProfilePoco>().HasMany(a => a.CompanyLocations).WithRequired(e => e.CompanyProfiles).HasForeignKey(e => e.Company);
+            modelBuilder.Entity<CompanyProfilePoco>().HasMany(a => a.CompanyJobs).WithRequired(e => e.CompanyProfiles).HasForeignKey(e => e.Company);
 
-
-
+            modelBuilder.Entity<SecurityLoginPoco>().HasMany(a => a.ApplicantProfiles).WithRequired(e => e.SecurityLogins).HasForeignKey(e => e.Login);            
+            modelBuilder.Entity<SecurityLoginPoco>().HasMany(a => a.SecurityLoginsLogs).WithRequired(e => e.SecurityLogins).HasForeignKey(e => e.Login);
+            modelBuilder.Entity<SecurityLoginPoco>().HasMany(a => a.SecurityLoginsRoles).WithRequired(e => e.SecurityLogins).HasForeignKey(e => e.Login);
+           
+            modelBuilder.Entity<SecurityRolePoco>().HasMany(a => a.SecurityLoginsRoles).WithRequired(e => e.SecurityRoles).HasForeignKey(e => e.Role);
+            
+            modelBuilder.Entity<SystemCountryCodePoco>().HasMany(a => a.ApplicantProfiles).WithRequired(e => e.SystemCountryCodes).HasForeignKey(e => e.Country);
+            modelBuilder.Entity<SystemCountryCodePoco>().HasMany(a => a.ApplicantWorkHistories).WithRequired(e => e.SystemCountryCodes).HasForeignKey(e => e.CountryCode);            
+            modelBuilder.Entity<SystemLanguageCodePoco>().HasMany(a => a.CompanyDescriptions).WithRequired(e => e.SystemLanguageCodes).HasForeignKey(e => e.LanguageId);
 
             modelBuilder.Entity<ApplicantEducationPoco>().Ignore(o => o.TimeStamp);
             modelBuilder.Entity<ApplicantJobApplicationPoco>().Ignore(o => o.TimeStamp);
@@ -39,6 +59,7 @@ namespace CareerCloud.EntityFrameworkDataAccess
             modelBuilder.Entity<SecurityLoginPoco>().Ignore(o => o.TimeStamp);
             modelBuilder.Entity<SecurityLoginsRolePoco>().Ignore(o => o.TimeStamp);
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
         public DbSet<ApplicantEducationPoco> ApplicantEducations { get; set; }
         public DbSet<ApplicantJobApplicationPoco> ApplicantJobApplications { get; set; }
